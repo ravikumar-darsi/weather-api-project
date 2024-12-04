ac0c0a0891426f6a099b05cccc2183ff919aa802
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-04 09:48:12 +0530

    jsonproperty was added to match the attributes

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/RealtimeWeather.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/RealtimeWeather.java
@@ -2,6 +2,8 @@ package com.skyapi.weatherforecast.common;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -20,11 +22,14 @@ public class RealtimeWeather {
 	private int temperature;
 	private int humidity;
 	private int precipitation;
+	
+	@JsonProperty("wind_speed")
 	private int windSpeed;
 	
 	@Column(length = 50)
 	private String status;
 	
+	@JsonProperty("last_updated")
 	private Date lastUpdated;
 	
 	@OneToOne
