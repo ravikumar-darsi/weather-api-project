ffb45428234fef7cab1b63615555013a3ccc9489
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-04 09:50:34 +0530

    utility class to get loc details of ip address

---
+++ WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/CommonUtility.java
@@ -0,0 +1,22 @@
+package com.skyapi.weatherforecast;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class CommonUtility {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(CommonUtility.class);
+
+	public static String getIpAddress(HttpServletRequest request) {
+		String ip = request.getHeader("X-FORWARED-FOR");
+
+		if (ip == null || ip.isEmpty()) {
+			ip = request.getRemoteAddr();
+		}
+		LOGGER.info("Client's IP Address: " + ip);
+
+		return ip;
+	}
+}
