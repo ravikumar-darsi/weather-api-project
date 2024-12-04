2c6d87cb63b0befc0a9df9da4aef93732b29844d
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-04 15:23:16 +0530

    validations was added to overcome the exceptions

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/RealtimeWeather.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/RealtimeWeather.java
@@ -1,7 +1,12 @@
 package com.skyapi.weatherforecast.common;
 
 import java.util.Date;
+import java.util.Objects;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -11,30 +16,42 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "realtime_weather")
 public class RealtimeWeather {
 	
 	@Id @Column(name = "location_code")
+	@JsonIgnore
 	private String locationCode;
 	
+	@Range(min = -50, max = 50, message = "Temperature must be in the range of -50 to 50 Celsius degree" )
 	private int temperature;
+	@Range(min = 0, max = 100, message = "Humidity must be in the range of 0 to 100 percentage" )
+
 	private int humidity;
+	
+	@Range(min = 0, max = 100, message = "Precipitation must be in the range of 0 to 100 percentage" )
 	private int precipitation;
 	
+	@Range(min = 0, max = 200, message = "Wind Speed must be in the range of 0 to 200 km/h" )
 	@JsonProperty("wind_speed")
 	private int windSpeed;
 	
 	@Column(length = 50)
+	@NotBlank(message = "Status must be empty")
+	@Length(min= 3, max = 50, message = "Status must be in between 3-50 characters")
 	private String status;
 	
 	@JsonProperty("last_updated")
+	@JsonIgnore
 	private Date lastUpdated;
 	
 	@OneToOne
 	@JoinColumn(name = "location_code")
 	@MapsId
+	@JsonIgnore
 	private Location location;
 
 	public String getLocationCode() {
@@ -101,6 +118,25 @@ public class RealtimeWeather {
 		this.locationCode = location.getCode();
 		this.location = location;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(locationCode);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RealtimeWeather other = (RealtimeWeather) obj;
+		return Objects.equals(locationCode, other.locationCode);
+	}
+	
+	
 	
 	
 }
