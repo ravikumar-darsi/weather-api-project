09c6f6b5037a09b3a26f385731d674a62178dc76
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-05 09:22:34 +0530

    Entity class for HourlyWeather

---
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/HourlyWeather.java
@@ -0,0 +1,83 @@
+package com.skyapi.weatherforecast.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "weather_hour")
+public class HourlyWeather {
+
+	@EmbeddedId
+	private HourlyWeatherId id = new HourlyWeatherId();
+
+	private int temperature;
+	private int precipitation;
+
+	@Column(length = 50)
+	private String status;
+
+	public HourlyWeatherId getId() {
+		return id;
+	}
+
+	public void setId(HourlyWeatherId id) {
+		this.id = id;
+	}
+
+	public int getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(int temperature) {
+		this.temperature = temperature;
+	}
+
+	public int getPrecipitation() {
+		return precipitation;
+	}
+
+	public void setPrecipitation(int precipitation) {
+		this.precipitation = precipitation;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public HourlyWeather temperature(int temp) {
+		setTemperature(temp);
+		return this;
+	}
+
+	public HourlyWeather id(Location location, int hour) {
+		this.id.setHourOfDay(hour);
+		this.id.setLocation(location);
+		return this;
+	}
+
+	public HourlyWeather precipitation(int precipitation) {
+		setPrecipitation(precipitation);
+		return this;
+	}
+
+	public HourlyWeather status(String status) {
+		setStatus(status);
+		return this;
+	}
+
+	public HourlyWeather location(Location location) {
+		this.id.setLocation(location);
+		return this;
+	}
+
+	public HourlyWeather hourOfDay(int hour) {
+		this.id.setHourOfDay(hour);
+		return this;
+	}
+}
