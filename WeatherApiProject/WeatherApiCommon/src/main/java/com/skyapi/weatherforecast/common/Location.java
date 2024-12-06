cd53fc6f17af654768a2afe11c25bbba1d991769
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-06 08:10:23 +0530

    Removed validationand added them in LocationDTO class

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import org.hibernate.validator.constraints.Length;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,58 +12,42 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-@SuppressWarnings("unused")
 @Entity
 @Table(name = "locations")
 public class Location {
 
 	@Column(length = 12, nullable = false, unique = true)
 	@Id
-	@NotNull(message = "Location code cannot be null")
-	@Length(min = 3, max = 12, message = "Location code must have 3-12 characters")
 	private String code;
 
 	@Column(length = 128, nullable = false)
-	@JsonProperty("city_name")
-	@NotNull(message = "City name cannot be null")
-	@Length(min = 3, max = 128, message = "City name must have 3-128 characters")
 	private String cityName;
 
 	@Column(length = 128)
-	@JsonProperty("region_name")
-	@Length(min = 3, max = 128, message = "Region name must have 3-128 characters")
 	private String regionName;
 
 	@Column(length = 64, nullable = false)
-	@JsonProperty("country_name")
-	@NotNull(message = "Country name cannot be null")
-	@Length(min = 3, max = 64, message = "Country name must have 3-64 characters")
 	private String countryName;
 
 	@Column(length = 2, nullable = false)
-	@JsonProperty("country_code")
-	@NotNull(message = "Country code cannot be null")
-	@Length(min = 2, max = 2, message = "Country code must have 2 characters")
 	private String countryCode;
 
 	private boolean enabled;
-
-	@JsonIgnore
+	
 	private boolean trashed;
-
+	
 	@OneToOne(mappedBy = "location", cascade = CascadeType.ALL)
 	@PrimaryKeyJoinColumn
-	@JsonIgnore
 	private RealtimeWeather realtimeWeather;
-
-	@OneToMany(mappedBy = "id.location", cascade = CascadeType.ALL)
-	private List<HourlyWeather> listHourlyWeather = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.location", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<HourlyWeather> listHourlyWeather = new ArrayList<>();
+
 	public Location() {
+	
 	}
+	
 	public Location(String cityName, String regionName, String countryName, String countryCode) {
 		super();
 		this.cityName = cityName;
@@ -162,15 +141,18 @@ public class Location {
 	public void setRealtimeWeather(RealtimeWeather realtimeWeather) {
 		this.realtimeWeather = realtimeWeather;
 	}
+
 	public List<HourlyWeather> getListHourlyWeather() {
 		return listHourlyWeather;
 	}
+
 	public void setListHourlyWeather(List<HourlyWeather> listHourlyWeather) {
 		this.listHourlyWeather = listHourlyWeather;
 	}
-
+	
 	public Location code(String code) {
 		setCode(code);
 		return this;
 	}
+
 }
