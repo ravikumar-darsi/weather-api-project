fa8e8193b55a82c2f826a0412a8ba9032d41bfc6
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-06 08:39:24 +0530

    added methods to catch LocationNotFoundException and ConstraintViolationException

--- WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/GlobalExceptionHandler.java
+++ WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/GlobalExceptionHandler.java
@@ -19,7 +19,10 @@ import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.skyapi.weatherforecast.location.LocationNotFoundException;
+
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -42,6 +45,62 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		return error;
 	}
 
+	@ExceptionHandler(BadRequestException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ErrorDTO handleBadRequestException(HttpServletRequest request, Exception ex) {
+		ErrorDTO error = new ErrorDTO();
+		
+		error.setTimestamp(new Date());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.addError(ex.getMessage());
+		error.setPath(request.getServletPath());
+		
+		LOGGER.error(ex.getMessage(), ex);
+		
+		return error;
+	}
+	
+	@ExceptionHandler(LocationNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ResponseBody
+	public ErrorDTO handleLocationNotFoundException(HttpServletRequest request, Exception ex) {
+		ErrorDTO error = new ErrorDTO();
+		
+		error.setTimestamp(new Date());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.addError(ex.getMessage());
+		error.setPath(request.getServletPath());
+		
+		LOGGER.error(ex.getMessage(), ex);
+		
+		return error;
+	}
+	
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ErrorDTO handleConstraintViolationException(HttpServletRequest request, Exception ex) {
+		ErrorDTO error = new ErrorDTO();
+		
+		ConstraintViolationException violationException = (ConstraintViolationException) ex;
+		
+		error.setTimestamp(new Date());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setPath(request.getServletPath());
+		
+		var constraintViolations = violationException.getConstraintViolations();
+		
+		constraintViolations.forEach(constraint -> {
+			error.addError(constraint.getPropertyPath() + ": " + constraint.getMessage());
+		});
+		
+		LOGGER.error(ex.getMessage(), ex);
+		
+		return error;
+	}
+	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
