e151911f20df0998022a1fb6aed9370eb5f1b1c1
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-06 08:13:19 +0530

    Creating custom class for BadRequestException

---
+++ WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/BadRequestException.java
@@ -0,0 +1,11 @@
+package com.skyapi.weatherforecast;
+
+@SuppressWarnings("serial")
+public class BadRequestException extends Exception {
+
+	public BadRequestException(String message) {
+		super(message);
+	}
+
+	
+}
