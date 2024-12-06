cbc82e297f0ae3611e2861053f4ed095657952ac
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-06 08:35:37 +0530

    added new constructor for custom exception

--- WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationNotFoundException.java
+++ WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationNotFoundException.java
@@ -1,10 +1,13 @@
 package com.skyapi.weatherforecast.location;
 
 @SuppressWarnings("serial")
-public class LocationNotFoundException extends Exception {
+public class LocationNotFoundException extends RuntimeException {
 
-	public LocationNotFoundException(String message) {
-		super(message);
+	public LocationNotFoundException(String locationCode) {
+		super("No location found with the given code: " + locationCode);
 	}
 
+	public LocationNotFoundException(String countryCode, String cityName) {
+		super("No location found with the given country code: " + countryCode + "and city name:" + cityName);
+	}
 }
