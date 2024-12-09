7cca1100a4cdb2b1a2675a0642cb3afe91d08a7e
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-09 21:47:11 +0530

    setter methods added

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/DailyWeather.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/DailyWeather.java
@@ -6,39 +6,91 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name= "weather_daily")
+@Table(name = "weather_daily")
 public class DailyWeather {
-  
-  @EmbeddedId
-  private DailyWeatherId id  = new DailyWeatherId();
 
-  private int minTemp;
-  private int maxTemp;
-  private int precipitation;
+	@EmbeddedId
+	private DailyWeatherId id = new DailyWeatherId();
 
-  @Column(length = 50)
-  private String status;
+	private int minTemp;
+	private int maxTemp;
+	private int precipitation;
 
-  public DailyWeatherId getId() {
-    return id;
-  }
+	@Column(length = 50)
+	private String status;
 
-  public int getMinTemp() {
-    return minTemp;
-  }
+	public DailyWeatherId getId() {
+		return id;
+	}
 
-  public int getMaxTemp() {
-    return maxTemp;
-  }
+	public void setId(DailyWeatherId id) {
+		this.id = id;
+	}
 
-  public int getPrecipitation() {
-    return precipitation;
-  }
+	public int getMinTemp() {
+		return minTemp;
+	}
 
-  public String getStatus() {
-    return status;
-  }
+	public void setMinTemp(int minTemp) {
+		this.minTemp = minTemp;
+	}
 
+	public int getMaxTemp() {
+		return maxTemp;
+	}
 
-  
+	public void setMaxTemp(int maxTemp) {
+		this.maxTemp = maxTemp;
+	}
+
+	public int getPrecipitation() {
+		return precipitation;
+	}
+
+	public void setPrecipitation(int precipitation) {
+		this.precipitation = precipitation;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public DailyWeather precipitation(int precipitation) {
+		setPrecipitation(precipitation);
+		return this;
+	}
+
+	public DailyWeather status(String status) {
+		setStatus(status);
+		return this;
+	}
+
+	public DailyWeather location(Location location) {
+		this.id.setLocation(location);
+		return this;
+	}
+	
+	public DailyWeather dayOfMonth(int day) {
+		this.id.setDayOfMonth(day);
+		return this;
+	}
+	
+	public DailyWeather month(int month) {
+		this.id.setMonth(month);
+		return this;
+	}
+	
+	public DailyWeather minTemp(int temp) {
+		setMinTemp(temp);
+		return this;
+	}
+	
+	public DailyWeather maxTemp(int temp) {
+		setMaxTemp(temp);
+		return this;
+	}
 }
