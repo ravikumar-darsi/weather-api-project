ecb0d2540486b0f41a60aa64e80816b5b89d7c35
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-10 15:39:31 +0530

    removed vaidation on Entity class and added them on DTO class

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/RealtimeWeather.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/RealtimeWeather.java
@@ -3,10 +3,6 @@ package com.skyapi.weatherforecast.common;
 import java.util.Date;
 import java.util.Objects;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.Range;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -16,42 +12,28 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "realtime_weather")
 public class RealtimeWeather {
 	
 	@Id @Column(name = "location_code")
-	@JsonIgnore
 	private String locationCode;
 	
-	@Range(min = -50, max = 50, message = "Temperature must be in the range of -50 to 50 Celsius degree" )
 	private int temperature;
-	@Range(min = 0, max = 100, message = "Humidity must be in the range of 0 to 100 percentage" )
-
 	private int humidity;
-	
-	@Range(min = 0, max = 100, message = "Precipitation must be in the range of 0 to 100 percentage" )
 	private int precipitation;
-	
-	@Range(min = 0, max = 200, message = "Wind Speed must be in the range of 0 to 200 km/h" )
-	@JsonProperty("wind_speed")
 	private int windSpeed;
 	
 	@Column(length = 50)
-	@NotBlank(message = "Status must not be empty")
-	@Length(min= 3, max = 50, message = "Status must be in between 3-50 characters")
 	private String status;
 	
 	@JsonProperty("last_updated")
-	@JsonIgnore
 	private Date lastUpdated;
 	
 	@OneToOne
 	@JoinColumn(name = "location_code")
 	@MapsId
-	@JsonIgnore
 	private Location location;
 
 	public String getLocationCode() {
@@ -135,8 +117,6 @@ public class RealtimeWeather {
 		RealtimeWeather other = (RealtimeWeather) obj;
 		return Objects.equals(locationCode, other.locationCode);
 	}
-	
-	
 	
 	
 }
