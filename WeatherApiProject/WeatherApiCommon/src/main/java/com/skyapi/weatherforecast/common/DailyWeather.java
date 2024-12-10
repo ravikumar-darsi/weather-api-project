c13824b8b0f138577f1ae9a5c6465bc20b4dcea1
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-10 15:36:53 +0530

    added ShallowCopy() method for delete operation while updating the data

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/DailyWeather.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/DailyWeather.java
@@ -1,5 +1,7 @@
 package com.skyapi.weatherforecast.common;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -11,14 +13,21 @@ public class DailyWeather {
 
 	@EmbeddedId
 	private DailyWeatherId id = new DailyWeatherId();
-
+	
 	private int minTemp;
 	private int maxTemp;
 	private int precipitation;
-
+	
 	@Column(length = 50)
 	private String status;
 
+	public DailyWeather getShallowCopy() {
+		DailyWeather copy = new DailyWeather();
+		copy.setId(this.getId());
+		
+		return copy;
+	}
+	
 	public DailyWeatherId getId() {
 		return id;
 	}
@@ -58,21 +67,21 @@ public class DailyWeather {
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
+	
 	public DailyWeather precipitation(int precipitation) {
 		setPrecipitation(precipitation);
 		return this;
 	}
-
+	
 	public DailyWeather status(String status) {
 		setStatus(status);
 		return this;
 	}
-
+	
 	public DailyWeather location(Location location) {
 		this.id.setLocation(location);
 		return this;
-	}
+	}	
 	
 	public DailyWeather dayOfMonth(int day) {
 		this.id.setDayOfMonth(day);
@@ -93,4 +102,29 @@ public class DailyWeather {
 		setMaxTemp(temp);
 		return this;
 	}
+
+	@Override
+	public String toString() {
+		return "DailyWeather [id=" + id + ", minTemp=" + minTemp + ", maxTemp=" + maxTemp + ", precipitation="
+				+ precipitation + ", status=" + status + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DailyWeather other = (DailyWeather) obj;
+		return Objects.equals(id, other.id);
+	}	
+	
+	
 }
