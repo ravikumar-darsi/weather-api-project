aa522b18f7306c6a6469d26184dd9037511d6335
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-10 15:45:43 +0530

    test methods includes all cases for Controller and Repository of DailyWeather

--- WeatherApiProject/WeatherApiService/src/test/java/com/skyapi/weatherforecast/daily/DailyWeatherRepositoryTests.java
+++ WeatherApiProject/WeatherApiService/src/test/java/com/skyapi/weatherforecast/daily/DailyWeatherRepositoryTests.java
@@ -2,6 +2,7 @@ package com.skyapi.weatherforecast.daily;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -20,43 +21,58 @@ import com.skyapi.weatherforecast.common.Location;
 @Rollback(false)
 public class DailyWeatherRepositoryTests {
 
-	@Autowired
-	private DailyWeatherRepository repository;
+	private @Autowired DailyWeatherRepository repo;
 	
 	@Test
 	public void testAdd() {
-		String locationCode= "DANA_VN";
+		String locationCode = "DANA_VN";
 		
 		Location location = new Location().code(locationCode);
 		
-		
 		DailyWeather forecast = new DailyWeather()
-						.location(location)
-						.dayOfMonth(16)
-						.month(7)
-						.minTemp(23)
-						.maxTemp(32)
-						.precipitation(40)
-						.status("Cloudy");
+				.location(location)
+				.dayOfMonth(16)
+				.month(7)
+				.minTemp(23)
+				.maxTemp(32)
+				.precipitation(40)
+				.status("Cloudy");	
 		
-		DailyWeather addedForecast = repository.save(forecast);
+		DailyWeather addedForecast = repo.save(forecast);
 		
 		assertThat(addedForecast.getId().getLocation().getCode()).isEqualTo(locationCode);
-		
 	}
 	
 	@Test
 	public void testDelete() {
-		String locationCode= "DELHI_IN";
+		String locationCode = "DELHI_IN";
 		
 		Location location = new Location().code(locationCode);
 		
 		DailyWeatherId id = new DailyWeatherId(16, 7, location);
 		
-		repository.deleteById(id);
+		repo.deleteById(id);
 		
-		Optional<DailyWeather> result = repository.findById(id);
+		Optional<DailyWeather> result = repo.findById(id);
 		
 		assertThat(result).isNotPresent();
+	}
+	
+	@Test
+	public void testFindByLocationCodeFound() {
+		String locationCode = "DELHI_IN";
+		
+		List<DailyWeather> dailyWeather = repo.findByLocationCode(locationCode);
+		
+		assertThat(dailyWeather).isNotEmpty();
+	}
+	
+	@Test
+	public void testFindByLocationCodeNotFound() {
+		String locationCode = "ABC_XYZ";
+		
+		List<DailyWeather> dailyWeather = repo.findByLocationCode(locationCode);
+		
+		assertThat(dailyWeather).isEmpty();
 	}
 }
