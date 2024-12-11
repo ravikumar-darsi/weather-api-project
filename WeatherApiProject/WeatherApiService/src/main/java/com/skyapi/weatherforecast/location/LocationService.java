77163ed57679e3e40704f3a1c93f684ca5e25c11
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-12-11 16:13:27 +0530

    Code Refactor

--- WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationService.java
+++ WeatherApiProject/WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationService.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.skyapi.weatherforecast.AbstractLocationService;
 import com.skyapi.weatherforecast.common.Location;
 
 @Service
 @Transactional
-public class LocationService {
+public class LocationService extends AbstractLocationService {
 
 	private LocationRepository repo;
 
@@ -36,21 +37,17 @@ public class LocationService {
 		 return location;
 	}
 
-	public Location update(Location locationInRequest)  {
+	public Location update(Location locationInRequest) {
 		String code = locationInRequest.getCode();
-
+		
 		Location locationInDB = repo.findByCode(code);
-
+		
 		if (locationInDB == null) {
 			throw new LocationNotFoundException(code);
 		}
-
-		locationInDB.setCityName(locationInRequest.getCityName());
-		locationInDB.setRegionName(locationInRequest.getRegionName());
-		locationInDB.setCountryCode(locationInRequest.getCountryCode());
-		locationInDB.setCountryName(locationInRequest.getCountryName());
-		locationInDB.setEnabled(locationInRequest.isEnabled());
-
+		
+		locationInDB.copyFieldsFrom(locationInRequest);
+		
 		return repo.save(locationInDB);
 	}
 
